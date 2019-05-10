5ad4c73f427e6637b7613df531e1d0af31d2c71b
AshkanAbd <kotksthegame@gmail.com>
2019-05-10 07:48:18 +0430

    Add %, support leading  for input, improve UX & UI

--- project1.asm
+++ project1.asm
@@ -4,20 +4,24 @@ include "emu8086.inc"
 
 .data
 
-num1_req_msg DB "Enter first number: $"
-num2_req_msg DB "Enter second number: $"  
-op_req_msg DB "Enter operator(+ - * / ): $" 
+num1_req_msg DB 13, 10, "Enter first number: $"
+num2_req_msg DB 13, 10, "Enter second number: $"  
+op_req_msg DB 13, 10, "Enter operator(+,-,*,/,%): $" 
 
-dot_msg DB "*10^-$"      
+dot_msg DB "x10^-$"      
 error_msg DB "ERROR$"
+quit_msg DB 13, 10, "Press ('Q'/'q') to quit$"
+continue_msg DB 13, 10, "Press other keys to continue", 13, 10,"$"
 
 num1_int DD 0H
 num1_dot DW 0H
-num1_fill DB 0H
+num1_fill DB 0H 
+num1_sign DB 1H
 
 num2_int DD 0H
 num2_dot DW 0H 
 num2_fill DB 0H
+num2_sign DB 1H
 
 res_int DD 0H
 res_dot DW 0H
@@ -30,8 +34,22 @@ start:
     MOV AX, @data
     MOV DS, AX     
                 
-    MOV Bl, 0H            
-    JMP main
+    MOV num1_fill, 0H
+    MOV num1_int, 0H
+    MOV num1_dot, 0H 
+    MOV num1_sign, 1H
+    
+    MOV num2_fill, 0H
+    MOV num2_int, 0H
+    MOV num2_dot, 0H
+    MOV num2_sign, 1H
+    
+    MOV res_int, 0H
+    MOV res_dot, 0H
+    
+    MOV op, 0H
+            
+    JMP clear_screen
     
 main:
     CMP num1_fill, 0H
@@ -41,13 +59,24 @@ main:
     JE req_num2 
     
     CMP op, 0H
-    JE req_op  
+    JE req_op
+    
+    MOV AX, num1_int
+    IMUL num1_sign
+    MOV num1_int, AX
+    
+    MOV AX, num2_int
+    IMUL num2_sign
+    MOV num2_int, AX  
     
     JMP find_res_dot
 
-req_num1:
-    LEA DX, num1_req_msg
+req_num1: 
+    LEA DX, quit_msg
     MOV AH, 09H
+    INT 21H    
+
+    LEA DX, num1_req_msg
     INT 21H
     
     MOV DI , 0H
@@ -60,13 +89,29 @@ get_num1:
     CMP AL, 0DH
     JE save_dot1
     
+    CMP AL, 51H
+    JE exit 
+    
+    CMP AL, 71H
+    JE exit
+    
     INC num1_fill
 
     CMP AL, 2EH
     JE find_dot1
     
+    MOV BL, AL
+    SUB BL, 2CH
+    CMP BL, num1_fill
+    JE change_sign1            
+    
     INC DI
     JMP add_int1
+    
+change_sign1:
+    MOV num1_sign, 0FFH
+    
+    JMP get_num1
 
 add_int1:
     MOV CL, AL                
@@ -95,8 +140,11 @@ save_dot1:
     JMP clear_screen    
     
 req_num2:
-    LEA DX, num2_req_msg
+    LEA DX, quit_msg
     MOV AH, 09H
+    INT 21H
+    
+    LEA DX, num2_req_msg
     INT 21H
     
     MOV DI, 0H
@@ -109,13 +157,29 @@ get_num2:
     CMP AL, 0DH
     JE save_dot2 
     
+    CMP AL, 51H
+    JE exit 
+    
+    CMP AL, 71H
+    JE exit
+    
     INC num2_fill
-
+    
     CMP AL, 2EH
-    JE find_dot2
+    JE find_dot2 
+    
+    MOV BL, AL
+    SUB BL, 2CH
+    CMP BL, num1_fill
+    JE change_sign2
  
     INC DI
-    JMP add_int2
+    JMP add_int2 
+    
+change_sign2:
+    MOV num2_sign, 0FFH
+    
+    JMP get_num2
 
 add_int2:
     MOV CL, AL                
@@ -143,9 +207,12 @@ save_dot2:
 
     JMP clear_screen     
     
-req_op:
-    LEA DX, op_req_msg
+req_op:     
+    LEA DX, quit_msg
     MOV AH, 09H
+    INT 21H
+    
+    LEA DX, op_req_msg
     INT 21H
     
     JMP get_op
@@ -155,11 +222,34 @@ get_op:
     INT 21H
     
     CMP op, 0H
-    JNE clear_screen
+    JNE clear_screen 
+ 
             
     MOV op, AL
     
-    JMP get_op
+    CMP op, 2BH
+    JE get_op
+    
+    CMP op, 2AH
+    JE get_op
+    
+    CMP op, 2DH
+    JE get_op
+    
+    CMP op, 2FH
+    JE get_op
+    
+    CMP op, 25H
+    JE get_op
+    
+    CMP AL, 51H
+    JE exit 
+    
+    CMP AL, 71H
+    JE exit
+                    
+    MOV op, 0H                
+    JMP clear_screen
         
 
 clear_screen:
@@ -171,6 +261,10 @@ clear_screen:
     JMP main
     
 prepare:
+    MOV AH, 02H
+    MOV DL, 0AH
+    INT 21H   
+    
     MOV AX, num1_int
     CALL print_num
  
@@ -280,7 +374,10 @@ calculate:
     JE multiply
     
     CMP op, 2FH
-    JE divide
+    JE divide  
+    
+    CMP op, 25H
+    JE mode
     
     JMP exit
     
@@ -313,7 +410,19 @@ divide:
     MOV BL, b.num2_int   
     
     IDIV BL
-    MOV res_int, AX
+    MOV b.res_int, AL
+    
+    JMP print    
+
+mode:
+    CMP num2_int, 0H
+    JE print_error
+
+    MOV AX, num1_int
+    MOV BL, b.num2_int   
+    
+    IDIV BL
+    MOV b.res_int, AH
     
     JMP print    
     
@@ -328,14 +437,35 @@ print:
     MOV AX, res_dot
     CALL print_num   
     
-    JMP exit
+    JMP restart
     
 print_error: 
     LEA DX, error_msg
     MOV AH, 09H
     INT 21H 
     
-    JMP exit
+    JMP restart
+    
+restart:
+    LEA DX, quit_msg
+    MOV AH, 09H
+    INT 21H
+    
+    LEA DX, continue_msg
+    MOV AH, 09H
+    INT 21H
+    
+    MOV AH, 01H
+    INT 21H
+    
+    CMP AL, 51H
+    JE exit 
+    
+    CMP AL, 71H
+    JE exit
+    
+    JMP start
+            
     
 exit:
     DEFINE_PRINT_NUM
